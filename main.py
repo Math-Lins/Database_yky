7bf34f9eead21ee135055d189b7a18ca1d006799
Diego Rodrigues <79650470+DiegoRodrigues76@users.noreply.github.com>
2024-05-07 15:25:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import os
+
+os.system('cls')
+
+meubd = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='34271573',
+    database=''
+)
+
+cursor = meubd.cursor()
+cursor.execute("SHOW DATABASES LIKE'meu_banco_de_dados'")
+
+resultado = cursor.fetchall()
+
+if resultado:
+    print(("O Banco de Dados já existe."))
+else:
+    print("Criando Banco de Dados...")
+    cursor.execute("CREATE DATABASE meu_banco_de_dados")
+
+    print("Banco de Dados criado com sucesso!")
