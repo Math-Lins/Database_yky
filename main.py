10749c01c0f2f7cc507f0b85eae8abf8d6d618be
Diego Rodrigues <79650470+DiegoRodrigues76@users.noreply.github.com>
2024-05-08 14:38:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,19 +6,38 @@ os.system('cls')
 meubd = mysql.connector.connect(
     host='localhost',
     user='root',
-    passwd='********',
+    passwd='34271573',
     database=''
 )
 
 cursor = meubd.cursor()
-cursor.execute("SHOW DATABASES LIKE'meu_banco_de_dados'")
+cursor.execute("SHOW DATABASES LIKE'crud'")
 
-resultado = cursor.fetchall()
+resultadoDB = cursor.fetchone()
 
-if resultado:
+if resultadoDB:
     print(("O Banco de Dados já existe."))
 else:
     print("Criando Banco de Dados...")
-    cursor.execute("CREATE DATABASE meu_banco_de_dados")
+    cursor.execute("CREATE DATABASE crud")
 
     print("Banco de Dados criado com sucesso!")
+
+cursor.execute("USE crud")
+cursor.execute("SHOW TABLES LIKE'clientes'")
+
+resultadoTable = cursor.fetchone()
+
+if resultadoTable:
+    print(("\nA Tabela já existe."))
+else:
+    print("\nCriando Tabela...")
+    cursor.execute("CREATE TABLE clientes(id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), email VARCHAR(255))")
+    print("Tabela criada com sucesso!")
+
+sql = "INSERT INTO clientes (nome, email) VALUES (%s,%s)"
+dados = ("Diego Sawyer", "dhrsawyer@teste.email.com")
+
+cursor.execute(sql, dados)
+meubd.commit()
+print("\nDados inseridos com sucesso!")
